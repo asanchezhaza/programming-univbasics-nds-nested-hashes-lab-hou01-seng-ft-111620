6f6197be9ed13842cf5ad675daf6600aa670c97a
Alan sanchez <asanchezhaza@gmail.com>
2020-10-21 05:21:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  array [constante,constante]
+  array [name,occupation]
   
   
   

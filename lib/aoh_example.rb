788d79a5fbc24b491e4c7a3bbd41f39a6638f477
Alan sanchez <asanchezhaza@gmail.com>
2020-10-21 06:52:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,7 +1,7 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio  Personality" }
+JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =     { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G =    { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G =   { name:  "Bruce Green", occupation: "Fan of Mildred" }
@@ -17,9 +17,9 @@ end
 
 def literal_aoh
   
-  array = [{ name:  "Don Gately", occupation: "Live-in Staff" },
+  array = [{ name:  "Don Gately", occupation: "Live-in Staff"   },
   
-            { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
             { name:  "Pat Monteseian", occupation: "Staff" },
             { name:  "Kate Gompert", occupation: "None" },
             { name:  "Bruce Green", occupation: "Fan of Mildred" },

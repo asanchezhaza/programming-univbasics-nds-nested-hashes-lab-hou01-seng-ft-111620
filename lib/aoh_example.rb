c690dcccaef6965312409c50facc2b0d2135c885
Alan sanchez <asanchezhaza@gmail.com>
2020-10-21 05:29:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,9 +20,9 @@ def literal_aoh
   array = [{ name:  "Don Gately", occupation: "Live-in Staff" }
   
             { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =     { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G =    { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G =   { name:  "Bruce Green", occupation: "Fan of Mildred" }
+            { name:  "Pat Monteseian", occupation: "Staff" }
+            { name:  "Kate Gompert", occupation: "None" }
+            { name:  "Bruce Green", occupation: "Fan of Mildred" }
   
   
   
